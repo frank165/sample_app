7a01df3ead5e762a20b65d3c1a4e21567d9a0bd1
yejunyue <yejunyue@gme.xm>
2016-07-20 13:46:52 +0800

    Static Pages Exercises Finish!

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   get 'staticpages/help'
 
-  get 'staticpages/about'  
+  get 'staticpages/about'
+
+  get 'staticpages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
